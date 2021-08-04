e7504183dcc758ce396c689658ab93a05fbeb415
Adelaide <adiainthesky@gmail.com>
2021-08-04 16:38:04 -0700

    all user routes fully functional

--- app/routes.py
+++ app/routes.py
@@ -13,15 +13,69 @@ trips_bp = Blueprint("trips", __name__, url_prefix="/trips")
 photos_bp = Blueprint("photos", __name__, url_prefix="/photos")
 # journal_entries_bp = Blueprint("journal_entries", __name__, url_prefix="/journal_entries")
 
+################  user routes ##################
+@users_bp.route("", methods=["POST"])
+def post_user():
+    request_body = request.get_json()
+    if not request_body:
+        return make_response("expected request body to be in JSON"), 400
+    elif not "name" in request_body.keys():
+        return make_response("expected 'name' to exist in the request body"), 400
+    else:
+        user = User(user_name=request_body["name"])
+        db.session.add(user)
+        db.session.commit()
+    return jsonify({"user": user.api_response()}), 201
+
+@users_bp.route("", methods=["GET"])
+def get_users():
+    # can sort here if i want
+    users = User.query.all()
+    users_response = [user.api_response() for user in users] 
+    return jsonify(users_response), 200        
+
+#######need to figure out how to handle a non-numeric trip_id input to avoid a 500error
+@users_bp.route("/<user_id>", methods=["GET"])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id, description=f'ID #{user_id} not found')
+    return jsonify({"user": user.api_response()}), 200    
+
+@users_bp.route("/<user_id>", methods=["PUT"])
+def put_user(user_id):
+    user = User.query.get_or_404(user_id, description=f'ID #{user_id} not found')
+    request_body = request.get_json()
+    if not request_body:
+        return make_response("expected request body to be in JSON"), 400
+    user.user_name = request_body.get("name",None)
+    db.session.commit()
+    return jsonify({"user": user.api_response()}), 200 
+
+##maybe add a patch  -- but how?
+
+@users_bp.route("/<user_id>", methods=["DELETE"])
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id, description=f'ID #{user_id} not found')
+    db.session.delete(user)
+    db.session.commit()
+    return make_response(f'"{user.user_name}" successfully deleted'), 200
+
+
+
+
+
+
+
+
+
+
+
+
 ################  trip routes ##################
 @trips_bp.route("", methods=["POST"])
 def post_trip():
     request_body = request.get_json()
     if not request_body:
-        return make_response(
-            # apparently it didnt like dict format here (but i could do it in dict for if i used .jsonify): {"details": "expected request body to be in JSON"}
-            "expected request body to be in JSON"
-        ), 400
+        return make_response("expected request body to be in JSON"), 400
     elif not "name" in request_body.keys():
         return make_response("expected 'name' to exist in the request body"), 400
     else:
@@ -38,7 +92,6 @@ def post_trip():
 
 @trips_bp.route("", methods=["GET"])
 def get_trips():
-    # can sort here if i want
     trips = Trip.query.all()
     trips_response = [trip.api_response() for trip in trips] 
     return jsonify(trips_response), 200        
@@ -112,20 +165,11 @@ def put_photo(photo_id):
     db.session.commit()
     return jsonify({"photo": photo.api_response()}), 200 
 
-
 ##maybe add a patch  -- but how?
-
-# @photos_bp.route("/<photo_id>", methods=["DELETE"])
-# def delete_photo(photo_id):
-#     photo = Photo.query.get_or_404(photo_id, description=f'ID #{photo_id} not found')
-#     db.session.delete(photo)
-#     db.session.commit()
-#     return make_response(f'"{photo.title}" successfully deleted'), 200    
-
 
 @photos_bp.route("/<photo_id>", methods=["DELETE"])
 def delete_photo(photo_id):
     photo = Photo.query.get_or_404(photo_id, description=f'ID #{photo_id} not found')
     db.session.delete(photo)
     db.session.commit()
-    return make_response(f'"Photo #{photo.photo_id}" successfully deleted'), 200    
+    return make_response(f'"Photo #{photo.photo_id}" successfully deleted'), 200
