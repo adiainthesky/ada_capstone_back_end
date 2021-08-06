6f8b2826586d48fb2fbce431fd4b8614076cdb8b
Adelaide <adiainthesky@gmail.com>
2021-08-05 17:01:44 -0700

    update env vars on app.yaml

--- main.py
+++ main.py
@@ -8,6 +8,54 @@ from dotenv import load_dotenv     #is this also needed for the above?
 db = SQLAlchemy()
 migrate = Migrate()
 
+# vars that i am getting from my environment (which here is app.yaml)
+db_user = os.environ.get('CLOUD_SQL_USERNAME')
+db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+
+def open_connection():
+    unix_socket = '/cloudsql/{}'.format(db_connection_name)
+    print(unix_socket)
+    try:
+        if os.environ.get('GAE_ENV') == 'standard':
+            conn = db.connect(user=db_user, password=db_password,
+                                unix_socket=unix_socket, db=db_name,
+                                cursorclass=db.cursors.DictCursor
+                                )
+    except Exception as e:
+    # except db.MySQLError as e:
+        print(e)
+
+    return conn
+
+
+
+
+
+# db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+
+# pool = db.create_engine(
+
+#     # Equivalent URL:
+#     # postgresql+pg8000://<db_user>:<db_pass>@/<db_name>?unix_sock=<socket_path>/<cloud_sql_instance_name>/.s.PGSQL.5432
+#     db.engine.url.URL.create(
+#         drivername="postgresql+pg8000",
+#         username=db_user,  # e.g. "my-database-user"
+#         password=db_pass,  # e.g. "my-database-password"
+#         database=db_name,  # e.g. "my-database-name"
+#         query={
+#             "unix_sock": "{}/{}/.s.PGSQL.5432".format(
+#                 db_socket_dir,  # e.g. "/cloudsql"
+#                 cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+#         }
+#     ),
+#     **db_config
+# )
+
+# if in dev use: env vars, else, use google cloud vars for prodcuton
+
 def create_app(test_config=None):
     print(os.environ)
     app = Flask(__name__)
